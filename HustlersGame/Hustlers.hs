522f533cb87d8666d832d7f9f3522013233b66ae
crespoma <98053532+crespoma@users.noreply.github.com>
2023-12-08 05:27:35 -0500

    small bugs and error handling left....:)

--- HustlersGame/Hustlers.hs
+++ HustlersGame/Hustlers.hs
@@ -1,7 +1,175 @@
-import System.IO  
+-- Hustler.hs
+
+import System.IO
+import Text.XHtml (action)
+import Control.Monad
+import Data.Maybe (fromJust, isJust, Maybe(Just))
+import Data.List
+import Control.Monad.Trans.Maybe (MaybeT)
+import Text.Read (Lexeme(String))
+
+
+
+ 
+data GameState = GameState
+  { backpack :: [String]
+  , wallet :: Int
+  , location :: String
+  , shops :: [(String, Int, [(String,Int,Int, Int)])]
+  }
+
+initialState :: GameState
+initialState = GameState
+  { backpack = ["napkin"]
+  , wallet = 10
+  , location = "Home"
+  , shops = [("Neighbor", 1, [("Cigarette",2,5,1), ("Vape",5,2,2), ("Crayons",5,1,10)])]
   
-main = do  
-    handle <- openFile "HowToPlay.txt" ReadMode  
-    contents <- hGetContents handle  
-    putStrLn contents  
-    hClose handle  
+  }
+
+displayState :: GameState -> IO ()
+displayState state = do
+  putStrLn $ "Backpack: " ++ show (backpack state)
+  putStrLn $ "Wallet : $" ++ show (wallet state)
+  putStrLn $ "Location: " ++ location state
+  putStrLn $ "Shops: " ++ show (map (\(name,_,_) -> name)(shops state))
+  putStrLn $ "Cost to Travel: 1mi = $" ++ show (costToTravel state 1) ++ "\n"
+
+costToTravel :: GameState -> Int -> Int
+costToTravel state distance = distance * 2
+
+main :: IO ()
+main = do
+  let loop gameState = do
+        putStr $ getPrompt gameState
+        action <- getLine
+        if action == "quit" || wallet gameState <= 0
+          then putStrLn "Exiting the game."
+          else do
+            
+            newState <- handleAction gameState action
+            displayState newState
+            loop newState
+
+      getPrompt :: GameState -> String
+      getPrompt newState =
+        case location newState of
+          "Home" -> "WAKE UP CHECKOUT THAT NAPKIN IN YOUR BACKPACK (type 'quit' to exit): "
+          "Neighbor" -> "Hey, my name is Adam Smasher, what can I do for you? (type 'leave' to travel or 'shop' to see items): "
+          _ -> "Where would you like to travel next? (type 'quit' to exit): "
+      
+      handleAction :: GameState -> String -> IO GameState
+      handleAction newState "quit" = return newState
+      handleAction newState "napkin" = do
+        howToPlayContent <- readFile "HowToPlay.txt"
+        putStrLn howToPlayContent
+        return newState  -- Return the original state
+      handleAction state "Neighbor" = do
+        let newState = travel state "Neighbor"
+        putStrLn $ "Traveled to " ++ "Neighbor"
+        displayState newState
+        return newState  
+        putStrLn "Hey, my name is Adam Smasher, what can I do for you?\n"
+        putStrLn "Options: 'leave' to travel , 'shop' to see items or 'sell' to sell items.\n"
+        action <- getLine
+        case action of
+          "leave" -> do
+            putStr "Where would you like to travel next? "
+            destination <- getLine
+            let newState = travel state destination
+            putStrLn $ "Traveled to " ++ destination
+            displayState newState
+            return newState
+          "shop" -> do
+            let shopItems = case lookup "Neighbor" (map (\(name, _, items) -> (name, items)) (shops state)) of
+                  Just items -> items
+                  Nothing    -> []
+            putStrLn $ "Items for sale in Neighbor: " ++ show shopItems
+            displayState newState
+            putStrLn $ "enter buy"
+            action <- getLine
+            case action of
+                "buy" -> do
+                    putStrLn $ "Which item would you like to buy?" ++ show shopItems
+                    item <- getLine
+                    let newState = buyItem newState item
+                    return newState
+          _ -> do
+            putStrLn "Invalid action. Please try again."
+            handleAction state "Neighbor"  -- Retry for invalid actions
+      handleAction state _ = do
+        displayState state  -- Display the state for other actions
+        return state
+
+  loop initialState
+
+
+travel :: GameState -> String -> GameState
+travel state destination =
+  if destination `elem` map (\(name,_,_) -> name) (shops state)
+    then
+      let travelCost = costToTravel state (distanceTo destination state)
+      in state { wallet = wallet state - travelCost, location = destination }
+    else
+      state
+
+distanceTo :: String -> GameState -> Int
+distanceTo destination state =
+  case lookup destination (map (\(name, dist, _) -> (name, dist)) (shops state)) of
+    Just distance -> distance
+    Nothing       -> 0  -- If the destination is not found, assume 0 distance
+
+
+buyItem :: GameState -> String -> GameState
+buyItem state item =
+  case lookup (location state) (map (\(name, _, items) -> (name, items)) (shops state)) of
+    Just shopItems ->
+      case find (\(name, _, _, _) -> name == item) shopItems of
+        Just (name, quantity, price, weight) ->
+          if price <= wallet state
+            then
+              let updatedState = updateShopInventory item state
+                  updatedShopsItems = getShopItemsFromState updatedState
+                  updatedShops = updateShopsList (shops state) (location state, 0, updatedShopsItems)
+              in updatedState { wallet = wallet state - price, backpack = item : backpack state, shops = updatedShops }
+            else
+              state -- Not enough funds to buy the item
+        Nothing ->
+          state -- Item not found in the shop
+    Nothing ->
+      state -- Shop not found in the current location
+
+getShopItemsFromState :: GameState -> [(String, Int, Int, Int)]
+getShopItemsFromState state =
+  case lookup (location state) (map (\(name, _, items) -> (name, items)) (shops state)) of
+    Just shopItems -> shopItems
+    Nothing        -> []
+
+updateShopInventory :: String -> GameState -> GameState
+updateShopInventory itemName state =
+    case lookup (location state) (map (\(name, _, items) -> (name, items)) (shops state)) of
+        Just shopItems ->
+            case find (\(name, _, _, _) -> name == itemName) shopItems of
+                Just (name, quantity, price, weight) ->
+                    let newQuantity = quantity - 1
+                        updatedShopItems = updateShopItemQuantity itemName newQuantity shopItems
+                        updatedShops = updateShopsList (shops state) (location state, 0, updatedShopItems)
+                    in state { shops = updatedShops }
+                Nothing -> state  -- Item not found in the shop
+        Nothing -> state  -- Shop not found in the current location
+updateShopsList :: [(String, Int, [(String, Int, Int, Int)])] -> (String, Int, [(String, Int, Int, Int)]) -> [(String, Int, [(String, Int, Int, Int)])]
+updateShopsList shopList updatedShop =
+  map (\(shopName, shopDist, items) ->
+    if shopName == fst3 updatedShop then updatedShop else (shopName, shopDist, items)
+  ) shopList
+  where
+    fst3 :: (a, b, c) -> a
+    fst3 (x, _, _) = x
+
+updateShopItemQuantity :: String -> Int -> [(String, Int, Int, Int)] -> [(String, Int, Int, Int)]
+updateShopItemQuantity itemName newQuantity shopItems =
+    map (\(name, currentQuantity, price, weight) ->
+        if name == itemName
+            then (name, newQuantity, price, weight)
+            else (name, currentQuantity, price, weight)
+    ) shopItems
---
+++ HustlersGame/Hustlersv2.hs
@@ -0,0 +1,269 @@
+import System.IO
+import Data.List (intercalate)
+import Control.Exception (bracket, evaluate, IOException, try)
+import Control.DeepSeq (NFData(rnf))
+
+data GameState = GameState
+  { backpack :: [String]
+  , wallet :: Int
+  , location :: String
+  } deriving (Show)
+
+initialState :: GameState
+initialState = GameState
+  { backpack = ["napkin", "map"]
+  , wallet = 10
+  , location = "Home"
+  }
+
+displayState :: GameState -> IO ()
+displayState state = do
+  putStrLn $ "Backpack: " ++ show (backpack state)
+  putStrLn $ "Wallet : $" ++ show (wallet state)
+  putStrLn $ "Location: " ++ location state
+
+formatShopItems :: String -> String
+formatShopItems shopContent =
+  "Items for sale in neighbor:\n" ++ shopContent
+
+readShopItems :: FilePath -> IO (Either String String)
+readShopItems filePath = do
+  result <- tryReadFile filePath
+  case result of
+    Left err -> return $ Left err
+    Right content -> return $ Right $ formatShopItems content
+
+tryReadFile :: FilePath -> IO (Either String String)
+tryReadFile filePath = do
+  result <- try $ readFile filePath
+  return $ case result of
+    Left (ex :: IOException) -> Left $ "Error reading file: " ++ show ex
+    Right content -> Right content
+
+updateShopFile :: FilePath -> String -> IO (Either String ())
+updateShopFile filePath updatedContent = do
+  result <- tryWriteFile filePath updatedContent
+  return $ case result of
+    Left err -> Left err
+    Right _ -> Right ()
+
+tryWriteFile :: FilePath -> String -> IO (Either String ())
+tryWriteFile filePath content = do
+  result <- try $ writeFile filePath content
+  return $ case result of
+    Left (ex :: IOException) -> Left $ "Error writing to file: " ++ show ex
+    Right _ -> Right ()
+
+buyItem :: GameState -> FilePath -> String -> Int -> IO GameState
+buyItem state shopFilePath item quantity = do
+  shopResult <- readShopItems shopFilePath
+  case shopResult of
+    Left err -> do
+      putStrLn $ "Error reading shop file: " ++ err
+      return state
+    Right shopContent -> do
+      writeResult <- tryWriteFile shopFilePath (updateShopQuantity shopContent item quantity)
+      case writeResult of
+        Left err -> do
+          let (updatedWallet, updatedBackpack) = updateWalletAndBackpack state item quantity
+          putStrLn $ "You bought " ++ show quantity ++ " " ++ item ++ "(s)."
+          let newState = state { wallet = updatedWallet, backpack = updatedBackpack }
+          displayState newState  -- Display the updated state
+          return newState
+        Right _ -> do
+          let (updatedWallet, updatedBackpack) = updateWalletAndBackpack state item quantity
+          putStrLn $ "You bought " ++ show quantity ++ " " ++ item ++ "(s)."
+          let newState = state { wallet = updatedWallet, backpack = updatedBackpack}
+          displayState newState  -- Display the updated state
+          return $ travel newState "neighbor"
+
+updateWalletAndBackpack :: GameState -> String -> Int -> (Int, [String])
+updateWalletAndBackpack state item quantity =
+  let itemInfo = getItemInfo item
+      updatedWallet = wallet state - itemValue itemInfo * quantity
+      updatedBackpack = replicate quantity item ++ backpack state
+  in (updatedWallet, updatedBackpack)
+
+updateShopQuantity :: String -> String -> Int -> String
+updateShopQuantity shopContent item quantity =
+  let linesList = lines shopContent
+      updatedLines = map (\line -> if line == itemLine then updatedItemLine else line) linesList
+      updatedShopContent = unlines updatedLines
+  in updatedShopContent
+  where
+    itemInfo = getItemInfo item
+    itemLine = item ++ "\n-----------------------------------"
+    updatedItemLine = item ++ "\n-----------------------------------\nQuantity of item [ " ++ show quantity ++ " ]\nBase value to purchase [ $" ++ show (itemValue itemInfo) ++ " ]\nRe-sell value [ $" ++ show (resellValue itemInfo) ++ " ]"
+
+getItemInfo :: String -> ItemInfo
+getItemInfo "cigarette" = ItemInfo 1 0.50
+getItemInfo "Liberty Pistol" = ItemInfo 50 25
+getItemInfo _ = ItemInfo 0 0  -- Default item info
+
+data ItemInfo = ItemInfo
+  { itemValue :: Int
+  , resellValue :: Double
+  }
+
+main :: IO ()
+main = loop initialState False False
+
+getPrompt :: GameState -> Bool -> Bool -> IO String
+getPrompt state hasReadNapkin hintDisplayed =
+  case location state of
+    "Home" -> return $ if hasReadNapkin || hintDisplayed
+                        then "Where you headed? "
+                        else "Hint: enter 'napkin' and hit enter to read it to checkout the rules. 'map' will show the distance from shop.\nWAKE UP CHOOM CHECKOUT THAT NAPKIN IN YOUR BACKPACK. (type 'quit' to exit): "
+    "neighbor" -> case wallet state <= 0 of
+      True -> return "You're out of money! Better luck next time. (type 'quit' to exit): "
+      False -> do
+        shopItemsResult <- readShopItems "neighborShop.txt"
+        case shopItemsResult of
+          Left err -> do
+            putStrLn $ "Error reading shop items: " ++ err
+            return ""
+          Right shopContent -> do
+            putStrLn shopContent
+            buyPromptResult <- buyPrompt
+            return buyPromptResult
+    _ -> return "Where would you like to travel next? (type 'quit' to exit): "
+
+loop :: GameState -> Bool -> Bool -> IO ()
+loop gameState hasReadNapkin hintDisplayed = do
+  putStr =<< getPrompt gameState hasReadNapkin hintDisplayed
+  action <- getLine
+  if action == "quit" || wallet gameState <= 0
+    then putStrLn "Exiting the game."
+    else do
+      newState <- handleAction gameState action hintDisplayed
+      let nextHasReadNapkin = hasReadNapkin || action == "napkin"
+          nextHintDisplayed = hintDisplayed || action == "napkin"
+      loop newState nextHasReadNapkin nextHintDisplayed
+
+
+handleAction :: GameState -> String -> Bool -> IO GameState
+handleAction state "quit" _ = return state
+handleAction state "napkin" _ = do
+  howToPlayContent <- readFile "HowToPlay.txt"
+  putStrLn howToPlayContent
+  return state
+handleAction state "map" _ = do
+    mapContent <- readFile "map.txt"
+    putStrLn mapContent
+    return state
+handleAction state "neighbor" _ = do
+  let newState = travel state "neighbor"
+  putStrLn "Hey, my name is Adam Smasher, what can I do for you?"
+  putStrLn "Options: 'leave' to travel, 'shop' to see items."
+  action <- getLine
+  case action of
+    "leave" -> do
+      handleAction newState "leave" False
+    "shop" -> do
+      putStrLn "buyin or sellin??"
+      buyOrSellAction <- getLine
+      case buyOrSellAction of
+        "buyin" -> do
+          shopItemsResult <- readShopItems "neighborShop.txt"
+          case shopItemsResult of
+            Left err -> do
+              putStrLn $ "Error reading shop items: " ++ err
+              return state
+            Right shopContent -> do
+              putStrLn shopContent
+              displayState newState
+              buyAction <- buyPrompt
+              newStateAfterBuy <- handleAction newState buyAction False
+              return newStateAfterBuy
+        "sellin" -> do
+          sellAction <- sellPrompt
+          newState <- sellItem state sellAction
+          handleAction newState "leave" False
+        _ -> do
+          putStrLn "Invalid action. Please try again."
+          return state
+    _ -> do
+      putStrLn "Invalid option. Please try again."
+      handleAction newState action False
+handleAction state "leave" _ = do
+  putStrLn "Where would you like to travel next?"
+  destination <- getLine
+  let newState = travel state destination
+  return newState
+handleAction state "cigarette" _ = do
+  putStrLn "How many would you like to buy?"
+  quantity <- readLn
+  newState <- buyItem state "neighborShop.txt" "cigarette" quantity
+  handleAction newState "leave" False
+handleAction state _ _ = do
+  return state
+
+
+buyPrompt :: IO String
+buyPrompt = do
+  putStrLn "GET CHROMED OUT CHOOM, What do you need"
+  input <- getLine
+  if input `elem` ["cigarette", "Liberty Pistol", "leave", "buyin", "sellin"]
+    then return input
+    else do
+      putStrLn "Invalid choice. Try again."
+      buyPrompt
+
+-- Implement the sellItem function
+sellItem :: GameState -> String -> IO GameState
+sellItem state item = do
+  case item `elem` backpack state of
+    False -> do
+      putStrLn $ "You don't have any " ++ item ++ "s to sell."
+      return state
+    True -> do
+      putStrLn $ "How many " ++ item ++ "s would you like to sell?"
+      quantity <- readLn
+      case quantity <= 0 || quantity > length (filter (== item) (backpack state)) of
+        True -> do
+          putStrLn "Invalid quantity. Please try again."
+          return state
+        False -> do
+          let sellValue = calculateSellValue item quantity
+              updatedWallet = wallet state + round sellValue
+              updatedBackpack = removeItems (backpack state) item quantity
+              
+              newState = state { wallet = updatedWallet, backpack = updatedBackpack}
+          putStrLn $ "You sold " ++ show quantity ++ " " ++ item ++ "(s) for $" ++ show sellValue ++ "."
+          displayState newState
+          return $ travel newState "home"
+
+-- Calculate the sell value based on the item
+calculateSellValue :: String -> Int -> Double
+calculateSellValue "cigarette" quantity = fromIntegral quantity * resellValue (getItemInfo "cigarette")
+calculateSellValue _ _ = 0
+
+
+-- Remove items from the backpack
+removeItems :: [String] -> String -> Int -> [String]
+removeItems [] _ _ = []
+removeItems (x:xs) item quantity
+  | x == item && quantity > 0 = removeItems xs item (quantity - 1)
+  | otherwise = x : removeItems xs item quantity
+
+-- Add a new prompt for selling
+sellPrompt :: IO String
+sellPrompt = do
+  putStrLn "What item would you like to sell?"
+  getLine
+
+travel :: GameState -> String -> GameState
+travel state destination =
+  case destination of
+    "neighbor" -> state { wallet = wallet state - 2, location = destination}
+    "home" -> state {wallet = wallet state - 2, location = destination}
+    _ -> state
+
+-- costToTravel :: GameState -> Int -> Int
+-- costToTravel state distance =
+--   case lookup (location state, distance) (map (\(src, dist, cost) -> ((src, dist), cost)) (travelCosts state)) of
+--     Just cost -> cost
+--     Nothing   -> 0  -- If the travel cost is not found, assume 0 cost
+
+-- travelCosts :: GameState -> [(String, Int, Int)]
+-- travelCosts state = map (\(name, dist, cost) -> (name, dist, cost)) (shops state)
